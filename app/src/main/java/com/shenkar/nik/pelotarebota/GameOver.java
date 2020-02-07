2f6460ebc97774cc4594a454a56f37f9a453d34c
Leonardo <leonardo.perello@est.fi.uncoma.edu.ar>
2020-02-07 11:40:32 -0300

    cambio de background Main Menu y agrego clase Game over pero sin que ande aún

---
+++ app/src/main/java/com/shenkar/nik/pelotarebota/GameOver.java
@@ -0,0 +1,36 @@
+package com.shenkar.nik.pelotarebota;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class GameOver extends Activity  {
+
+    private Button startAgain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        startAgain = (Button)findViewById(R.id.play);
+
+        startAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(GameOver.this, PelotaRebota.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
+
